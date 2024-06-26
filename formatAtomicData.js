3f52fa7e2f1fde95ade86616aeb1d179b2d6c262
Andrew Luhring <andrew@superlegit.fm>
2024-06-25 21:07:35 -0500

    adjusts formatAtomicData to delete more properties

--- formatAtomicData.js
+++ formatAtomicData.js
@@ -1,9 +1,9 @@
 import dataset from './data/ModernAtomic.json' with { type: "json" };
 import * as fs from 'node:fs/promises';
 
+
 for(let cardName in dataset.data){
 	let card = dataset.data[cardName];
-
 	if(card.length > 1){
 		for(let cardindex in card){
 			let subcard = card[cardindex];
@@ -12,6 +12,10 @@ for(let cardName in dataset.data){
 			delete subcard.purchaseUrls;
 			delete subcard.rulings;
 			delete subcard.printings;
+			delete subcard.edhrecRank;
+			delete subcard.edhrecSaltiness;
+			delete subcard.firstPrinting;
+			delete subcard.identifiers;
 		}
 	} else {
 		let subcard = card[0];
@@ -20,14 +24,17 @@ for(let cardName in dataset.data){
 		delete subcard.purchaseUrls;
 		delete subcard.rulings;
 		delete subcard.printings;
-
+		delete subcard.edhrecRank;
+		delete subcard.edhrecSaltiness;
+		delete subcard.firstPrinting;
+		delete subcard.identifiers;
 	}
 }
 
 
 (async function main(){
 	try{
-		await fs.writeFile('./noForeignModernAtomic.json', JSON.stringify(dataset.data));
+		await fs.writeFile('./noForeignModernAtomic-rust.json', JSON.stringify(dataset.data));
 		console.log('good job dude');
 	} catch(err){
 		console.error('hey bro calm down')
